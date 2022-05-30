79af7223450755153a967b646844d3cb49c644d6
alina <alina.bregvadze@gmail.com>
2022-05-30 21:46:17 +0400

    tasks ui

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,40 @@
+<template>
+	<header>
+		<h1>{{ title }}</h1>
+		<ButtonComp
+			@btn-click="$emit('toggle-add-task')"
+			:text="showAddTask ? 'Close' : 'Add Task'"
+			:color="showAddTask ? 'red' : 'green'"
+		/>
+		<!-- <ButtonComp text="delete" color="red" />
+		<ButtonComp text="update Task" color="blue" /> -->
+	</header>
+</template>
+
+<script>
+import ButtonComp from "./ButtonComp";
+
+export default {
+	name: "HeaderComp",
+	props: {
+		// title: {
+		// 	type: String,
+		// 	default: "hello world",
+		// },
+		title: String,
+		showAddTask: Boolean,
+	},
+	components: {
+		ButtonComp,
+	},
+};
+</script>
+
+<style scoped>
+header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	margin-bottom: 40px;
+}
+</style>
---
+++ src/components/TasksComp.vue
@@ -0,0 +1,23 @@
+<template>
+	<div v-for="task in tasks" :key="task.id">
+		<TaskComp
+			@toggle-reminder="$emit('toggle-reminder', task.id)"
+			@delete-task="$emit('delete-task', task.id)"
+			:task="task"
+		/>
+	</div>
+</template>
+
+<script>
+import TaskComp from "./TaskComp";
+export default {
+	name: "TasksComp",
+	props: {
+		tasks: Array,
+	},
+	components: {
+		TaskComp,
+	},
+	emits: ["delete-task", "toggle-reminder"],
+};
+</script>
